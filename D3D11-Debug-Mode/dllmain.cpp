bd037a310a8127f31d98d90a41cc4e413ca2f12b
The Ænema (Hamid.Memar) <94260384+TheAenema@users.noreply.github.com>
2022-01-23 01:18:07 +0330

    D3D11 Debug Mode for Unity DX11
    A Wrapper for Unity's DirectX 11 to Activate Debug Mode, This is useful for Unity Native Rendering Plugin development.
    
    [ Working Now ] 
    Golden Hint : Sleep enough :|

--- D3D11-Debug-Mode/dllmain.cpp
+++ D3D11-Debug-Mode/dllmain.cpp
@@ -1,22 +1,23 @@
 #include <Windows.h>
+#include <iostream>
 
 // D3D11 Typedefs
 typedef HRESULT(*D3D11CreateDeviceFunc)(uintptr_t pAdapter, int DriverType, HMODULE Software,
 	UINT Flags, uintptr_t pFeatureLevels, UINT FeatureLevels, UINT SDKVersion,
 	void** ppDevice, uintptr_t pFeatureLevel, void** ppImmediateContext);
 
-typedef enum D3D11_CREATE_DEVICE_FLAG 
+typedef enum D3D11_CREATE_DEVICE_FLAG
 {
-	D3D11_CREATE_DEVICE_SINGLETHREADED,
-	D3D11_CREATE_DEVICE_DEBUG,
-	D3D11_CREATE_DEVICE_SWITCH_TO_REF,
-	D3D11_CREATE_DEVICE_PREVENT_INTERNAL_THREADING_OPTIMIZATIONS,
-	D3D11_CREATE_DEVICE_BGRA_SUPPORT,
-	D3D11_CREATE_DEVICE_DEBUGGABLE,
-	D3D11_CREATE_DEVICE_PREVENT_ALTERING_LAYER_SETTINGS_FROM_REGISTRY,
-	D3D11_CREATE_DEVICE_DISABLE_GPU_TIMEOUT,
-	D3D11_CREATE_DEVICE_VIDEO_SUPPORT
-};
+	D3D11_CREATE_DEVICE_SINGLETHREADED = 0x1,
+	D3D11_CREATE_DEVICE_DEBUG = 0x2,
+	D3D11_CREATE_DEVICE_SWITCH_TO_REF = 0x4,
+	D3D11_CREATE_DEVICE_PREVENT_INTERNAL_THREADING_OPTIMIZATIONS = 0x8,
+	D3D11_CREATE_DEVICE_BGRA_SUPPORT = 0x20,
+	D3D11_CREATE_DEVICE_DEBUGGABLE = 0x40,
+	D3D11_CREATE_DEVICE_PREVENT_ALTERING_LAYER_SETTINGS_FROM_REGISTRY = 0x80,
+	D3D11_CREATE_DEVICE_DISABLE_GPU_TIMEOUT = 0x100,
+	D3D11_CREATE_DEVICE_VIDEO_SUPPORT = 0x800
+} 	D3D11_CREATE_DEVICE_FLAG;
 
 #pragma region Proxy
 struct d3d11_dll {
@@ -85,11 +86,17 @@ extern "C" {
 		void** ppDevice, uintptr_t pFeatureLevel, void** ppImmediateContext)
 	{ 
 		D3D11CreateDeviceFunc funOriginal = (D3D11CreateDeviceFunc)d3d11.oD3D11CreateDevice;
-
-		// Add Debug Flag
 		UINT altredFlags = Flags;
-		altredFlags |= D3D11_CREATE_DEVICE_DEBUG;
-		altredFlags |= D3D11_CREATE_DEVICE_DEBUGGABLE;
+
+
+		printf("------------------------------- Input Device Flag : %d\n", altredFlags);
+
+		if (altredFlags == 32)
+		{
+			// Add Debug Flag
+			altredFlags |= D3D11_CREATE_DEVICE_DEBUG;
+		}
+
 		return funOriginal(pAdapter, DriverType, Software, altredFlags, pFeatureLevels, FeatureLevels, SDKVersion, ppDevice, pFeatureLevel, ppImmediateContext);
 	}
 
